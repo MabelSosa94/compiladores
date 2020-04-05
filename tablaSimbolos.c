40c32df4fea1ad506f66b19a70ffc704b52e7a3d
MabelSosa94 <63166323+MabelSosa94@users.noreply.github.com>
2020-04-05 14:04:53 -0400

    Update tablaSimbolos.c

--- tablaSimbolos.c
+++ tablaSimbolos.c
@@ -5,12 +5,10 @@ entrada *tabla;				//declarar la tabla de simbolos
 int tamTabla=TAMHASH;		//utilizado para cuando se debe hacer rehash
 int elems=0;				//utilizado para cuando se debe hacer rehash
 
-int h(const char* k, int m)
-{
+int h(const char* k, int m){
 	unsigned h=0,g;
 	int i;
-	for (i=0;i<strlen(k);i++)
-	{
+	for (i=0;i<strlen(k);i++){
 		h=(h << 4) + k[i];
 		if ( (g=h&0xf0000000) ){
 			h=h^(g>>24);
@@ -20,19 +18,15 @@ int h(const char* k, int m)
 	return h%m;
 }
 
-void initTabla()
-{	
+void initTabla(){	
 	int i=0;
-	
 	tabla=(entrada*)malloc(tamTabla*sizeof(entrada));
-	for(i=0;i<tamTabla;i++)
-	{
+	for(i=0;i<tamTabla;i++){
 		tabla[i].compLex=-1;
 	}
 }
 
-int esprimo(int n)
-{
+int esprimo(int n){
 	int i;
 	for(i=3;i*i<=n;i+=2)
 		if (n%i==0)
@@ -40,8 +34,7 @@ int esprimo(int n)
 	return 1;
 }
 
-int siguiente_primo(int n)
-{
+int siguiente_primo(int n){
 	if (n%2==0)
 		n++;
 	for (;!esprimo(n);n+=2);
@@ -50,15 +43,13 @@ int siguiente_primo(int n)
 }
 
 //en caso de que la tabla llegue al limite, duplicar el tamaño
-void rehash()
-{
+void rehash(){
 	entrada *vieja;
 	int i;
 	vieja=tabla;
 	tamTabla=siguiente_primo(2*tamTabla);
 	initTabla();
-	for (i=0;i<tamTabla/2;i++)
-	{
+	for (i=0;i<tamTabla/2;i++){
 		if(vieja[i].compLex!=-1)
 			insertar(vieja[i]);
 	}		
@@ -66,14 +57,12 @@ void rehash()
 }
 
 //insertar una entrada en la tabla
-void insertar(entrada e)
-{
+void insertar(entrada e){
 	int pos;
 	if (++elems>=tamTabla/2)
 		rehash();
 	pos=h(e.lexema,tamTabla);
-	while (tabla[pos].compLex!=-1)
-	{
+	while (tabla[pos].compLex!=-1){
 		pos++;
 		if (pos==tamTabla)
 			pos=0;
@@ -82,12 +71,10 @@ void insertar(entrada e)
 
 }
 //busca una clave en la tabla, si no existe devuelve NULL, posicion en caso contrario
-entrada* buscar(const char *clave)
-{
+entrada* buscar(const char *clave){
 	int pos;
 	pos=h(clave,tamTabla);
-	while(tabla[pos].compLex!=-1 && strcmp(tabla[pos].lexema,clave)!=0 )
-	{
+	while(tabla[pos].compLex!=-1 && strcmp(tabla[pos].lexema,clave)!=0 ){
 		pos++;
 		if (pos==tamTabla)
 			pos=0;
@@ -95,75 +82,30 @@ entrada* buscar(const char *clave)
 	return &tabla[pos];
 }
 
-void insertTablaSimbolos(const char *s, int n)
-{
+void insertTablaSimbolos(const char *s, int n){
 	entrada e;
 	strcpy(e.lexema,s);
 	// sprintf(e.lexema,s);
 	e.compLex=n;
 	insertar(e);
 }
-
-void initTablaSimbolos()
-{
+//Simbolos Json simple.
+void initTablaSimbolos(){
 	int i;
 	const char *vector[]={
-		"program",
-		"type",
-		"var",
-		"array",
-		"begin",
-		"end",
-		"do",
-		"to",
-		"downto",
-		"then",
-		"of",
-		"function",
-		"procedure", 
-		"integer", 
-		"real", 
-		"boolean", 
-		"char", 
-		"for", 
-		"if", 
-		"else", 
-		"while", 
-		"repeat", 
-		"until", 
-		"case", 
-		"record", 
-		"writeln",
-		"write",
-		"const"
+		"literal_cadena",
+		"literal_num" 
 	};
- 	for (i=0;i<28;i++)
-	{
+ 	for (i=0;i<2;i++){
 		insertTablaSimbolos(vector[i],i+256);
 	}
-	insertTablaSimbolos(",",',');
-	insertTablaSimbolos(".",'.');
-	insertTablaSimbolos(":",':');
-	insertTablaSimbolos(";",';');
-	insertTablaSimbolos("(",'(');
-	insertTablaSimbolos(")",')');
-	insertTablaSimbolos("[",'[');
-	insertTablaSimbolos("]",']');
-	insertTablaSimbolos("true",BOOL);
-	insertTablaSimbolos("false",BOOL);
-	insertTablaSimbolos("not",NOT);
-	insertTablaSimbolos("<",OPREL);
-	insertTablaSimbolos("<=",OPREL);
-	insertTablaSimbolos("<>",OPREL);
-	insertTablaSimbolos(">",OPREL);
-	insertTablaSimbolos(">=",OPREL);
-	insertTablaSimbolos("=",OPREL);
-	insertTablaSimbolos("+",OPSUMA);
-	insertTablaSimbolos("-",OPSUMA);
-	insertTablaSimbolos("or",OPSUMA);
-	insertTablaSimbolos("*",OPMULT);
-	insertTablaSimbolos("/",OPMULT);
-	insertTablaSimbolos("div",OPMULT);
-	insertTablaSimbolos("mod",OPMULT);
-	insertTablaSimbolos(":=",OPASIGNA);
+	insertTablaSimbolos(",",COMA);
+	insertTablaSimbolos(":",DOS_PUNTOS);
+	insertTablaSimbolos("[",L_CORCHETE);
+	insertTablaSimbolos("]",R_CORCHETE);
+	insertTablaSimbolos("{",L_LLAVE);
+	insertTablaSimbolos("}",R_LLAVE);
+	insertTablaSimbolos("true",PR_TRUE);
+	insertTablaSimbolos("false",PR_FALSE);
+    insertTablaSimbolos("null",PR_NULL); 	
 }
